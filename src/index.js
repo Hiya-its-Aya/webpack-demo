b46b5e74ca2d8ffaf008894ea98e179db3bfd429
ayamoosa123 <ayamoosa98@gmail.com>
2022-07-12 16:39:40 -0700

    template webpack

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  element.appendChild(myIcon)
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
 //-------------------------------------------------------------------//
 
 // import your function
